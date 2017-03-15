cb1923d626110bf72f3b66149aad720337a61299
Chase Walden <chase_walden@me.com>
2017-03-15 15:57:55 -0700

    Typo

--- php/DBConnector.php
+++ php/DBConnector.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1); // Enable error logging
 
-class DBController {
+class DBConnector {
 	private $conn;
 
 	public function __construct($db, $host = 'localhost', $port = 3306, $user = 'root', $pass = 'root') {
