42a62c60dff3bf42abbb6ca818b26fe173b0b595
Chase Walden <chase_walden@me.com>
2017-03-15 15:57:04 -0700

    added some initial files

---
+++ php/DBConnector.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1); // Enable error logging
+
+class DBController {
+	private $conn;
+
+	public function __construct($db, $host = 'localhost', $port = 3306, $user = 'root', $pass = 'root') {
+		$this->conn = new mysqli($host, $user, $pass, $db, $port);
+	}
+
+
+	public function query($title, $q) {
+		// escape checking here to prevent injections
+		$res = $this->conn->query($q);
+
+    // TODO: Turn result into array...
+
+	}
+
+	public function __destruct() {
+		$this->conn->close();
+	}
+
+}
+
+?>
