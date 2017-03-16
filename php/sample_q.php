89233432b892b1b28a6aa3b48413fd5202e7cea3
Chase Walden <chase_walden@me.com>
2017-03-15 20:19:15 -0700

    left some stuff in

--- php/sample_q.php
+++ php/sample_q.php
@@ -4,12 +4,8 @@ require_once('DBConnector.php'); // import DBConnector
 
 header("Content-type:application/json"); // set file type to json!
 
-// $dad = $_GET['dad']; // get argument from url
-
 $postdata = file_get_contents("php://input");
 $params = json_decode($postdata);
-
-// var_dump($params);
 
 // sterilize the arguments
 
@@ -17,7 +13,6 @@ $conn = new DBConnector('304_db');
 
 $table = $conn->query("SELECT * FROM `ChildOf` WHERE name_father NOT LIKE '$params->dad'");
 
-sleep(5);
 
 echo json_encode($table);
 
