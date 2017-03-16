b4704787e954ea273b28a4cd5bab6ae41112cf2f
Chase Walden <chase_walden@me.com>
2017-03-16 01:03:58 -0700

    Added comments to improve readability

--- js/main.js
+++ js/main.js
@@ -13,38 +13,96 @@ function closeNav() {
 }
 
 // Angular Stuff
+// Create Angular Module
 var app = angular.module('GoT_Reference', ["ngRoute"]);
 
+// Create $page attribute
+app.factory('$page', function() {
+   var base_title = 'GoT Reference';
+   var title = 'GoT Reference';
+   return {
+     title: function() { return title; },
+     setTitle: function(newTitle) {
+        if (newTitle === "") {
+          title = base_title;
+        } else {
+          title = base_title + " - " + newTitle;
+        }
+      }
+   };
+});
+
+// add $page to the root scope
+app.run(function($rootScope, $page){
+  $rootScope.$page = $page;
+});
+
+// Setup templates for each query
 app.config(function($routeProvider) {
    $routeProvider
+    // <a href="#sample">link</a>
     .when("/sample", {
         templateUrl: "views/sample_q.html",
         controller: "SampleQController",
         url: ""
     })
+    // add more views here
+    // default to the coverpage
     .otherwise({
         templateUrl : "views/coverpage.html",
+        controller: "CoverPageController",
         url:""
     });
 });
 
-app.controller("SampleQController", function($scope, $http){
+// Controller for CoverPage view
+app.controller("CoverPageController", function($scope, $page){
+  $page.setTitle(""); // Set title
+});
+
+// Controller for SampleQ view
+app.controller("SampleQController", function($scope, $http, $page){
+  $page.setTitle("Sample"); // Set title
+
+  // default loading div and result table to hidden
   $scope.show_loading = false;
   $scope.show_result = false;
 
+  // submit function
+    // <... ng-click="submit_form()">
   $scope.submit_form = function () {
-    $scope.show_loading = true;
-    $scope.show_result = false;
+    // TODO: turn the hidden div's into Angular components
+      // error component
+      // spoiler component
+      // result table component
 
+    $scope.show_loading = true; // show loading div
+    $scope.show_result = false; // make sure result table is hidden
+
+    // send request to server
     $http.post("php/sample_q.php", {'dad': $scope.dad_name}).then(function success(res){
-      $scope.show_loading = false;
-      $scope.show_result = true;
-      $scope.result = res.data;
-    }, function error(res){
+      // when we get data back
 
+      // TODO: handle this
+      // if result
+        // if we get a valid result
+
+      $scope.show_loading = false; // hide loading div
+      $scope.show_result = true; // show result table
+      $scope.result = res.data; // populate result table
+
+      // else if spoiler
+        // if we get a spoiler, display the message
+      // else if error
+        // if we get an error, display the error
+      // else
+        // we shouldn't get this
+        // need to display an error
+
+    }, function error(res){
+      // if there was an error establishing connection,
+      // display an error
     });
   }
-
-
 
 });
--- php/sample_q.php
+++ php/sample_q.php
@@ -4,16 +4,17 @@ require_once('DBConnector.php'); // import DBConnector
 
 header("Content-type:application/json"); // set file type to json!
 
-$postdata = file_get_contents("php://input");
-$params = json_decode($postdata);
+$postdata = file_get_contents("php://input"); // get POST input (will be JSON from our UI)
+$params = json_decode($postdata); // Decode the JSON into something PHP can handle
 
-// sterilize the arguments
+// TODO: sterilize the arguments
 
-$conn = new DBConnector('304_db');
+$conn = new DBConnector('304_db'); // Open a connection to the DB
 
+// gets resulting query array
 $table = $conn->query("SELECT * FROM `ChildOf` WHERE name_father NOT LIKE '$params->dad'");
 
-
+// convert PHP array to JSON and output
 echo json_encode($table);
 
 ?>
